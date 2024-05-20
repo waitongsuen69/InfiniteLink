09bbc58e51e1137541e176946964f67b9a543db4
Husky69 <waitongsuen@outlook.com>
2024-05-20 21:31:40 +0930

    finish change

--- backend/config/db.js
+++ backend/config/db.js
@@ -10,9 +10,19 @@ const connectDB = async () => {
       });
       console.log('MongoDB Connected');
     } catch (err) {
-      console.error('Failed to connect to MongoDB', err);
-      // Exit process with failure code
-      process.exit(1);
+      console.error('Failed to connect to MongoDB using mongodb://mongodb:27017/myDatabase');
+      try {
+        // Fallback to the secondary URI if the primary connection fails
+        await mongoose.connect('mongodb://localhost:27017/yourDatabase', {
+          useNewUrlParser: true,
+          useUnifiedTopology: true,
+        });
+        console.log(`MongoDB connected at mongodb://localhost:27017/yourDatabase`);
+      } catch (err) {
+        console.error(`Failed to connect to secondary MongoDB URI: mongodb://localhost:27017/yourDatabase`);
+        // console.error(err.message);
+        process.exit(1); // Exit the process with failure
+      }
     }
 
 };
